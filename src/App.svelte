565036dcbfd9de363b4f77ca00f4e7057412baa8
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2020-03-22 11:01:28 +0100

    design improvments?

--- src/App.svelte
+++ src/App.svelte
@@ -82,6 +82,9 @@
                             </h2>
                             <small>
                                 <span class="source">
+                                    <svg class="icon icon-external">
+                                        <use xlink:href="#icon-external"></use>
+                                    </svg>
                                     {$data[`post_${i+1}_source`]}
                                 </span>
                                 <span class="timestamp">
@@ -95,11 +98,11 @@
                         <main>
                             <p>{$data[`post_${i+1}_text`]}</p>
                         </main>
-                        <footer>
-                        <span class="btn">
-                            {$data[`link_label`]}
-                        </span>
-                        </footer>
+<!--                        <footer>-->
+<!--                        <span class="btn">-->
+<!--                            {$data[`link_label`]}-->
+<!--                        </span>-->
+<!--                        </footer>-->
                     </article>
                 </a>
             {/each}
@@ -148,6 +151,11 @@
             <g fill="none" fill-rule="evenodd">
                 <circle cx="7.5" cy="7.5" fill="#9c9c9c" r="7.5"/>
                 <path d="m4 7.634h4.088l3.053-3.134" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.395"/>
+            </g>
+        </symbol>
+        <symbol id="icon-external" viewBox="0 0 24 24">
+            <g fill="#9c9c9c" fill-rule="evenodd">
+                <path d="M17 13v6c0 0.276-0.111 0.525-0.293 0.707s-0.431 0.293-0.707 0.293h-11c-0.276 0-0.525-0.111-0.707-0.293s-0.293-0.431-0.293-0.707v-11c0-0.276 0.111-0.525 0.293-0.707s0.431-0.293 0.707-0.293h6c0.552 0 1-0.448 1-1s-0.448-1-1-1h-6c-0.828 0-1.58 0.337-2.121 0.879s-0.879 1.293-0.879 2.121v11c0 0.828 0.337 1.58 0.879 2.121s1.293 0.879 2.121 0.879h11c0.828 0 1.58-0.337 2.121-0.879s0.879-1.293 0.879-2.121v-6c0-0.552-0.448-1-1-1s-1 0.448-1 1zM10.707 14.707l9.293-9.293v3.586c0 0.552 0.448 1 1 1s1-0.448 1-1v-6c0-0.136-0.027-0.265-0.076-0.383s-0.121-0.228-0.216-0.323c-0.001-0.001-0.001-0.001-0.002-0.002-0.092-0.092-0.202-0.166-0.323-0.216-0.118-0.049-0.247-0.076-0.383-0.076h-6c-0.552 0-1 0.448-1 1s0.448 1 1 1h3.586l-9.293 9.293c-0.391 0.391-0.391 1.024 0 1.414s1.024 0.391 1.414 0z"></path>
             </g>
         </symbol>
     </defs>
